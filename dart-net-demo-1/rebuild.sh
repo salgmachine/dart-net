374f2513c24a11946906a25213dcc41d3853b967
salgmachine <salgmachine@bcfh.me>
2019-03-01 20:06:25 -0500

    fixes

--- dart-net-demo-1/rebuild.sh
+++ dart-net-demo-1/rebuild.sh
@@ -2,5 +2,5 @@
 cd /home/paperspace/dart-net
 git pull
 ## mvn clean install -DskipTests=true -f /home/paperspace/dart-net/dart-img-generator/pom.xml
-mvn clean install -DskipTests=true -f /home/paperspace/dart-net/dart-net-demo-1/pom.xml
+mvn clean install -DskipTests=true -Pcuda -f /home/paperspace/dart-net/dart-net-demo-1/pom.xml
 /home/paperspace/jdk8/bin/java -jar -Ddartnet.input=/home/paperspace/dart-net/dart-net
